f0e2b4068bda0ab6d4368d71b1d804fbd2ef9616
etcroot <etcroot@protonmail.com>
2022-01-12 14:32:14 +0100

    Initial Upload

---
+++ src/main/java/dev/etcroot/pingkick/PingKick.java
@@ -0,0 +1,41 @@
+package dev.etcroot.pingkick;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class PingKick extends JavaPlugin implements Listener {
+
+    int maxPing = getConfig().getInt("max-ping");
+    String msg = getConfig().getString("kick-msg");
+
+    @EventHandler
+    public void CheckPings(PlayerJoinEvent e) {
+        for(Player p : getServer().getOnlinePlayers()) {
+            int ping = p.getPing();
+            if(ping > maxPing) {
+                e.getPlayer().kickPlayer(ChatColor.RED + msg);
+            }
+        }
+    }
+
+    @Override
+    public void onLoad() {
+        saveDefaultConfig();
+    }
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        saveDefaultConfig();
+    }
+}
